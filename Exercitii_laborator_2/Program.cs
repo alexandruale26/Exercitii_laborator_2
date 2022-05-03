47df1217a8a0dcc388170886768f10ab819b3332
alexandruale26 <alexandru_ale26@yahoo.com>
2022-05-03 10:13:25 +0300

    Add project files.

---
+++ Exercitii_laborator_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercitii_laborator_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!1111111");
+        }
+    }
+}
