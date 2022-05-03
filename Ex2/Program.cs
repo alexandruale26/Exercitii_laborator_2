adaa1c02f8340fe52c5125fec29568b38020fa64
alexandruale26 <alexandru_ale26@yahoo.com>
2022-05-03 12:40:50 +0300

    2

--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -6,7 +6,58 @@ namespace Ex2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!222222");
+            // Să se scrie un program care citeşte de la tastatură un şir de n numere naturale
+            // şi determină media aritmetică a celor pare, n fiind citit de la tastatra.
+
+            Console.WriteLine("Cate numere are sirul ?");
+            int lungimeSir = int.Parse(Console.ReadLine());
+
+            int[] sir = new int[lungimeSir];
+            if (lungimeSir < 2)
+            {
+                Console.WriteLine("Sirul nu poate avea mai putin de 2 numere");
+                return;
+            }
+
+            Console.WriteLine("Introduceti numerele");
+            for (int i = 0; i < sir.Length; i++)
+            {
+                sir[i] = int.Parse(Console.ReadLine());
+
+                if (sir[i] <= 0)
+                {
+                    Console.WriteLine("Numerele nu trebuie sa fie mai mici decat 0");
+                    return;
+                }
+            }
+
+            int total = 0, numerePare = 0;
+            double mediaAritmetica = 0;
+
+            for (int i = 0; i < sir.Length; i++)
+            {
+                if (sir[i] % 2 == 0)
+                {
+                    total += sir[i];
+                    numerePare++;
+                }
+                else
+                {
+                    total += 0;
+                }
+            }
+
+            mediaAritmetica = (double)total / numerePare;
+
+            if (total == 0)
+            {
+                Console.WriteLine("Nu ati introdus numere pare");
+            }
+            else
+            {
+                Console.WriteLine($"Media aritmetica a celor {numerePare} numere pare este  -> {mediaAritmetica}");
+            }
+
         }
     }
 }
--- Exercitii_laborator_2/Program.cs
+++ Exercitii_laborator_2/Program.cs
@@ -6,7 +6,26 @@ namespace Ex1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!1111111");
+            // Scrieti un program care va afisa factorialul
+            // unui numar n, n fiind citit de la tastatura.
+
+            Console.WriteLine("Introduceti numarul");
+            int n = int.Parse(Console.ReadLine());
+
+            int factorial = 1;
+
+            if (n < 0)
+            {
+                Console.WriteLine("Numarul nu poate fi negativ");
+                return;
+            }
+
+            for (int i = n; i > 0; i--)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine($"Rezultatul este  -> {factorial}");
         }
     }
 }
